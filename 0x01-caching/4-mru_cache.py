d0b3df4ef97536678c8106302b880ae5799fecfe
R-oxy <aronmangati@gmail.com>
2024-06-26 11:41:53 -0700

    task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -10,25 +10,25 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """ Initialize """
         super().__init__()
-        self.keys = []
+        self.order = []
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key is None or item is None:
             return
         if key in self.cache_data:
-            self.keys.remove(key)
+            self.order.remove(key)
         self.cache_data[key] = item
-        self.keys.append(key)
+        self.order.append(key)
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            mru_key = self.keys.pop()
+            mru_key = self.order.pop(-2)
             del self.cache_data[mru_key]
             print(f"DISCARD: {mru_key}")
 
     def get(self, key):
         """ Get an item by key """
         if key in self.cache_data:
-            self.keys.remove(key)
-            self.keys.append(key)
+            self.order.remove(key)
+            self.order.append(key)
             return self.cache_data[key]
         return None
